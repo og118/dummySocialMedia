3ad3673036bbcd5a0dfaab6aadc678b439e8aabe
og118 <63297841+og118@users.noreply.github.com>
2020-06-15 13:11:53 +0530

    further improved app.js (added security and sanitation methods)

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-const express = require('express')
+const fs = require('fs');
+const express = require('express');
+const helmet = require('helmet')
+const monogSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const morgan = require('morgan');
+
+
+
 const app = express();
+// MIDDLEWARES 
+// Security HTTP headers
+app.use(helmet());
+
+// Development Logging
+if(process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Body Parser, reading data from body to req.body
+app.use(express.json({limit: '10kb'}));
+
+// Data Sanitization against NoSQL query injection
+app.use(monogSanitize());
+
+// Data Sanitization against XSS
+app.use(xssClean());
+
+// Prevent Parameter Pollution
+app.use(hpp());
+
+// rate limiter
+const limiter = rateLimit({
+    max: 1000,
+    windowMs: 60*60*1000, 
+    message: 'Too many requests from this IP, try again next hour'
+})
+app.use('/api', limiter);
 
 module.exports = app;
+
--- server.js
+++ server.js
@@ -9,3 +9,11 @@ const app = require('./app');
 const server = app.listen(3000, () =>{
     console.log('App running on port 3000');
 }); 
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! Shutting Down');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
+});
